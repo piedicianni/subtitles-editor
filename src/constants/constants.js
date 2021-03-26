8845e363240f8842ee66354f1cb037b250612ede
piedicianni <acdc85@gmail.com>
2021-03-26 20:36:55 +0100

    Seek video when edit start or end point

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,9 @@
 const INFO_JSON = 'https://run.mocky.io/v3/92eb87c3-5907-46d7-96df-a2c0cb7b21cc';
 const SUBTITLE_LIST = 'https://run.mocky.io/v3/a739bc4a-d6f4-464d-8951-eee6b03cdf5c';
+const VIDEO_URL = 'https://video-test-temp.s3-eu-west-1.amazonaws.com/index.m3u8';
 
 export {
     INFO_JSON,
-    SUBTITLE_LIST
+    SUBTITLE_LIST,
+    VIDEO_URL
 };
--- src/containers/VideoArea.js
+++ src/containers/VideoArea.js
@@ -1,24 +1,42 @@
-import { useContext, useRef } from 'react';
-import Player from '../components/Player/Player';
+import { useContext, useEffect, useRef } from 'react';
+import ReactPlayer from 'react-player';
 import Nouislider from "nouislider-react";
 import "nouislider/distribute/nouislider.css";
 import { EditorContext } from './Editor';
+import { VIDEO_URL } from '../constants/constants';
 
 function VideoArea(props) {
     const { isEditingItem, secIn, setSecIn, secOut, setSecOut } = useContext(EditorContext);
-    const sliderRef = useRef(null);
+    const playerRef = useRef(null);
+
+    useEffect(() => {
+        if(secIn === 0) return;
+        playerRef.current.seekTo(secIn);
+    }, [secIn]);
+
+    useEffect(() => {
+        if(secOut === 0) return;
+        console.log(playerRef?.current)
+        playerRef.current.seekTo(secOut);
+    }, [secOut]);
+
+    useEffect(() => {
+        if(!isEditingItem) return;
+        // playerRef.current.props.playing = false;
+    }, [isEditingItem]);
 
     return (
         <div className='video-area'>
-            <Player
-                source='https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8'
+            <ReactPlayer
+                ref={playerRef}
+                controls
+                url={VIDEO_URL}
                 /* width={props.width}
                 height={props.height} */ />
             {isEditingItem && (
                 <div className='slider'>
                     <Nouislider
-                        instanceRef={sliderRef}
-                        range={{ min: 0, max: 598 }}
+                        range={{ min: 0, max: parseFloat(props.duration) }}
                         start={[secIn, secOut]}
                         onChange={(values, handle) => {
                             const setFn = handle === 0 ? setSecIn : setSecOut;
